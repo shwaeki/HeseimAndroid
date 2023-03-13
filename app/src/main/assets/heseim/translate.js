facde7b2d15360877c7ef8917889ed6435872764
Mohamad Shwaeki <shwaeki@gmail.com>
2023-03-13 21:57:59 +0200

    11

--- app/src/main/assets/heseim/translate.js
+++ app/src/main/assets/heseim/translate.js
@@ -70,24 +70,64 @@ if(char_mapping_arabic[old_char] == undefined)
     }
 }
 
-function correctString(old_string){
-new_string = ""
-for(i=0;i<old_string.length;i++){
-    new_string = new_string+correctChar(old_string[i])
+function changeArabicToHebrew(text){
+    var new_text = ""
+    for(i=0;i<text.length;i++){
+        new_text = new_text+correctChar(text[i])
+    }
+    return new_text;
 }
-return new_string;
+
+
+/*
+********************** Get Arabic Names File **********************
+*/
+ArabicNames = {};
+function readJsonFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
 }
-console.log(correctString("احمد"));
-console.log(correctString("محمد"));
-console.log(correctString("سامي"));
-console.log(correctString("هاني"));
-console.log(correctString("محمود"));
-console.log(correctString("سامر"));
-console.log(correctString("هناء"));
-console.log(correctString("فداء"));
-console.log(correctString("رامة"));
-console.log(correctString("طارق"));
-console.log(correctString("غادة"));
-console.log(correctString("سارة"));
-console.log(correctString("موسى"));
-console.log(correctString("شامي"));
+
+
+function translateToArabic(txt) {
+    //  console.log(ArabicNames);
+    txt.split(" ").forEach(function (name) {
+        if (name && ArabicNames[name] && ArabicNames[name] !== undefined) {
+        //    console.log(name + '  -  ' + ArabicNames[name]);
+            txt = txt.replace(name, ArabicNames[name]);
+        }
+    });
+    return txt
+}
+
+
+
+
+eventTypeListArabic = {
+    "הוספת ילד/ה לנסיעה": "اضافة طالب الى السفرية",
+    "הסעה מאחרת": "تأخر في السفرية",
+    "התנהגות שלילית של תלמיד/נהג": "سلوك الطالب / السائق السلبي",
+    "מסלול ארוך": "المسار يأخذ الكثير من الوقت",
+    "רכב לא תקין/לא בטיחותי": "مركبة غير جيدة / غير آمنة",
+    "איחור מבוגר אחראי לקבלת התלמיד/מסירת התלמיד": "تأخر اهل الطالب في  استلام / توصيل الطالب",
+    "אירוע חריג - אחר": "حدث غير عادي - آخر",
+};
+
+
+notCollectedReasonListArabic = {
+    "התלמיד איחר והנהג המשיך בנסיעה": "تأخر الطالب واستمر السائق في القيادة",
+    "התלמיד לא מגיע ללימודים": "لا يأتي الطالب إلى المدرسة",
+    "התלמיד יגיע עצמאית למוסד": "سيأتي الطالب إلى المؤسسة بشكل مستقل",
+    "התלמיד לא נוסע במסלול": "الطالب لا ينتمي الى هذه السفرية",
+    "אחר – הקלדת טקסט חופשי": "سبب اخر",
+};
+
+
+console.log("START TRANSLATE FILE");
